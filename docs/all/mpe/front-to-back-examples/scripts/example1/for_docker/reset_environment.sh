d20131c48da42049cf44178681ad83d73fa1c407
Sergey Rodionov <astroseger@gmail.com>
2019-01-21 16:17:44 +0300

    fix fron-to-back-example1

--- docs/all/mpe/front-to-back-examples/scripts/example1/for_docker/reset_environment.sh
+++ docs/all/mpe/front-to-back-examples/scripts/example1/for_docker/reset_environment.sh
@@ -35,21 +35,19 @@ nohup ./node_modules/.bin/ganache-cli --mnemonic 'gauge enact biology destroy no
 # III. remove old snet-cli configuration
 rm -rf ~/.snet
 
-# IV. Configure SNET-CLI.
-
-# Create First identity (snet-user = first ganache)
-# We will automatically create new configuration file with default values
-snet identity create snet-user key --private-key 0xc71478a6d0fe44e763649de0a0deb5a080b788eefbbcf9c6f7aef0dd5dbd67e0
-
-# Add local network and switch to it
+# add local Ethereum network to the snet configuration with the name "local".
 snet network create local http://localhost:8545
-snet network local
 
-# set correct ipfs endpoint
+# Create First identity (snet-user = first ganache identity)
+snet identity create snet-user rpc --network local
+
+# switch to snet-user (we will switch automatically to local network)
+snet identity snet-user
+
+# switch to local ipfs endpoint
 snet set  default_ipfs_endpoint http://localhost:5002
 
-
-# Configure contract addresses for local network (it will not be necessary for kovan or mainnet! )
+# Configure contract addresses for local network (for kovan addresess are already configured!)
 snet set current_singularitynettoken_at 0x6e5f20669177f5bdf3703ec5ea9c4d4fe3aabd14
 snet set current_registry_at            0x4e74fefa82e83e0964f0d9f53c68e03f7298a8b2
 snet set current_multipartyescrow_at    0x5c7a4290f6f8ff64c69eeffdfafc8644a4ec3a4e
