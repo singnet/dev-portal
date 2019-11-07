fabb76d4108bdfe18f6fdcc3d05a28156cd15d34
Vitaly Bogdanov <vsbogd@gmail.com>
2019-11-07 16:56:36 +0300

    Export 7000 port from docker

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name snet-local-env -p 5002:5002 -p 8545:8545 -ti vsbogd/snet-local-env
+docker run --name snet-local-env -p 5002:5002 -p 8545:8545 -p 7000:7000 -ti vsbogd/snet-local-env
