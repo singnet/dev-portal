4026db38cb26b49d19ea849839a1daf1c39a7993
Vitaly Bogdanov <vsbogd@gmail.com>
2019-11-08 15:56:42 +0300

    Publish docker to singularitynet repo

--- docker_build.sh
+++ docker_build.sh
@@ -2,5 +2,5 @@
 
 TAG=2.0.2
 
-docker build --build-arg TAG=${TAG} -t vsbogd/snet-local-env:${TAG} .
-docker tag vsbogd/snet-local-env:${TAG} vsbogd/snet-local-env:latest
+docker build --build-arg TAG=${TAG} -t singularitynet/snet-local-env:${TAG} .
+docker tag vsbogd/snet-local-env:${TAG} singularitynet/snet-local-env:latest
