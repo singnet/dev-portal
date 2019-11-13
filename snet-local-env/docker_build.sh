069dfec1f8147f736223ad0c975bef17845fc895
Vitaly Bogdanov <vsbogd@gmail.com>
2019-11-13 17:12:20 +0300

    Fix docker repo name

--- snet-local-env/docker_build.sh
+++ snet-local-env/docker_build.sh
@@ -3,4 +3,3 @@
 TAG=2.0.2
 
 docker build --build-arg TAG=${TAG} -t singularitynet/snet-local-env:${TAG} .
-docker tag vsbogd/snet-local-env:${TAG} singularitynet/snet-local-env:latest
--- snet-local-env/docker_run.sh
+++ snet-local-env/docker_run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 5002:5002 -p 8545:8545 -p 7000:7000 -ti vsbogd/snet-local-env:latest
+docker run -p 5002:5002 -p 8545:8545 -p 7000:7000 -ti singularitynet/snet-local-env:latest
