50685fbaa6d00595a840e0eece4c79ebec7cfc9b
Vitaly Bogdanov <vsbogd@gmail.com>
2019-11-13 19:24:53 +0300

    Add default container name

--- snet-local-env/start.sh
+++ snet-local-env/start.sh
@@ -2,4 +2,6 @@
 DIR=$(dirname $0)
 
 . ${DIR}/tag
-docker run -p 5002:5002 -p 8545:8545 -p 7000:7000 -ti singularitynet/snet-local-env:${TAG}
+docker run --name 'snet-local-env' \
+	-p 5002:5002 -p 8545:8545 -p 7000:7000 \
+	-ti singularitynet/snet-local-env:${TAG}
