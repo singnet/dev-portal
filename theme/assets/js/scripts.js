86e4a5b538693e336cd6e8e0b030a826b95d492e
Sujith Varma K <sujith.varma@nirman.io>
2020-02-10 17:51:17 +0530

    Adding AI publisher blurred image

--- theme/assets/js/scripts.js
+++ theme/assets/js/scripts.js
@@ -186,11 +186,12 @@
 
 
 
-
+    $(".snet-card_maincontents img").css({"cursor":"pointer"});
+    
     // Get the modal
     var modal = document.getElementById("snetModal");
 
-// When the user clicks anywhere outside of the modal, close it
+    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
