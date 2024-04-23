58da0b07d95520c26cff41dc38892d30d71f5cbb
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-04-23 13:41:35 +0300

    [SPT-337] test left menu click

--- theme/assets/js/hide-menu-script.js
+++ theme/assets/js/hide-menu-script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const leftSectionCollapsedSize = 'col-md-1';
 const leftSectionNonCollapsedSize = 'col-md-3';
 
@@ -61,6 +63,7 @@ function showLeftSection() {
 }
 
 const collapsedElement = document.getElementById('titleLeftMenu');
+console.log('collapsedElement: ', collapsedElement);
 collapsedElement.addEventListener('click', function () {
     const isComponentHiden = leftSection.classList.contains(HIDE_MENU_CLASS);
     isComponentHiden ? showLeftSection() : hideLeftSection();
