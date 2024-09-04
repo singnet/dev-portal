ac78b74b240b60204e11add27564b51c9b0ed8b7
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-09-04 16:21:55 +0300

    [SPT-525] updated button hover in navigation card

--- components/Home/NavigationCard.vue
+++ components/Home/NavigationCard.vue
@@ -62,117 +62,116 @@ methods: {
     gap: 10px;
 }
 
-.card-redirect{
+.navigation-card .image-container img{
+    border-radius: 20px;
+    width: 265px;
+    height: 135px;
+    object-fit: cover;
+}
+
+.navigation-card p {
+    padding: 0;
+    margin: 0;
+}
+
+.navigation-card svg {
+    width: 24px;
+    height: 18px;
+}
+
+.navigation-card .card-body {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+}
+
+.navigation-card .card-title {
+    margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+}
+
+.navigation-card .card-title-text {
+    overflow: hidden;
+}
+
+.navigation-card .card-title-text p {
+    font-size: 22px;
+    line-height: 30px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+}
+
+.navigation-card .card-title-icon {
+    flex: 0 0 auto;
+    margin-right: 5px;
+}
+
+.navigation-card .card-description {
+    margin-bottom: 10px;
+}
+
+.navigation-card .card-description p {
+    font-size: 16px;
+    line-height: 24px;
+}
+
+.card-redirect p {
+    line-height: unset;
+    display: flex;
+}
+
+svg {
+    margin-top: 5%;
+}
+
+.card-redirect {
+    border-radius: 50px;
+    position: relative;
+    padding: 15px 20px;
     color: var(--vp-c-black);
     justify-content: center;
     align-items: center;
     display: flex;
 }
 
-.dark .card-redirect svg{
-fill: #FAFAFA !important;
+.card-redirect:hover, 
+.card-redirect::before  {
+    box-shadow: 0 0 10px var(--vp-accent-border);
+
 }
 
-.card-redirect svg{
-fill: black !important;
+.dark .card-redirect svg {
+    fill: #FAFAFA !important;
+}
+
+.card-redirect svg {
+    fill: black !important;
 }
 
 .dark .card-redirect{
-color: var(--vp-c-white)
-}
-.navigation-card:hover .card-redirect{
-text-decoration: underline;
-}
-
-.navigation-card .image-container img{
-border-radius: 20px;
-width: 265px;
-height: 135px;
-object-fit: cover;
-}
-
-.navigation-card p {
-padding: 0;
-margin: 0;
-}
-
-.navigation-card svg {
-width: 24px;
-height: 18px;
-}
-
-.navigation-card .card-body {
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-height: 100%;
-}
-
-.navigation-card .card-title {
-margin-bottom: 10px;
-display: flex;
-align-items: center;
-}
-
-.navigation-card .card-title-text {
-overflow: hidden;
-}
-
-.navigation-card .card-title-text p {
-font-size: 22px;
-line-height: 30px;
-overflow: hidden;
-white-space: nowrap;
-text-overflow: ellipsis;
-}
-
-.navigation-card .card-title-icon {
-flex: 0 0 auto;
-margin-right: 5px;
-}
-
-.navigation-card .card-description {
-margin-bottom: 10px;
-}
-
-.navigation-card .card-description p {
-font-size: 16px;
-line-height: 24px;
-}
-.card-redirect p {
-line-height: unset;
-display: flex;
-}
-
-svg {
-margin-top: 5%;
-}
-
-.card-redirect {
-text-shadow: 10px;
-border-radius: 25px;
-padding: 8px;
-position: relative;
-padding: 15px 20px;
+    color: var(--vp-c-white);
 }
 
 .card-redirect::before {
-content: "";
-position: absolute;
-top: 0;
-left: 0;
-right: 0;
-bottom: 0;
-border-radius: 50px; 
-border: 2px solid transparent;
-background: linear-gradient(0deg, rgba(158, 0, 255, 1),
-rgba(212, 76, 206, 1),
-rgba(40, 40, 218, 1)) border-box;
--webkit-mask:
-    linear-gradient(#fff 0 0) padding-box, 
-    linear-gradient(#fff 0 0);
--webkit-mask-composite: destination-out;
-mask-composite: exclude;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 50px; 
+    border: 2px solid transparent;
+    background: linear-gradient(0deg, rgba(158, 0, 255, 1),
+    rgba(212, 76, 206, 1),
+    rgba(40, 40, 218, 1)) border-box;
+    -webkit-mask:
+        linear-gradient(#fff 0 0) padding-box, 
+        linear-gradient(#fff 0 0);
+    -webkit-mask-composite: destination-out;
+    mask-composite: exclude;
 }
 
 @media (max-width: 640px) {
