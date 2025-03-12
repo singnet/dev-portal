a2996b423430fec1c2768a66d669f02972bc6400
Andrew Serbin <serbinadrian@outlook.com>
2025-03-12 17:56:48 +0300

    [SDP-10] Updated README with Accordion syntax

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,12 @@
-import SpriteIcon from "../../components/Common/SpriteIcon.vue";
+import DefaultTheme from "vitepress/theme";
 import Home from "../../components/Home/Home.vue";
 import Video from "../../components/Common/Video.vue";
-import ImageViewer from "../../components/Common/ImageViewer.vue";
-import NavigationControl from "../../components/NavigationControl/NavigationControl.vue";
-import FeedBackForm from "../../components/Common/FeedBackForm.vue";
 import Footer from "../../components/Footer/Footer.vue";
-import DefaultTheme from "vitepress/theme";
+import SpriteIcon from "../../components/Common/SpriteIcon.vue";
+import ImageViewer from "../../components/Common/ImageViewer.vue";
+import FeedBackForm from "../../components/Common/FeedBackForm.vue";
+import AccordionItem from "../../components/Common/AccordionItem.vue";
+import NavigationControl from "../../components/NavigationControl/NavigationControl.vue";
 import type { Theme } from "vitepress";
 import { h } from "vue";
 import "./style.css";
@@ -27,6 +28,7 @@ export default {
         app.component("Home", Home);
         app.component("Video", Video);
         app.component("SpriteIcon", SpriteIcon);
-        app.component("ImageViewer", ImageViewer)
+        app.component("ImageViewer", ImageViewer);
+        app.component("AccordionItem", AccordionItem);
     },
 } satisfies Theme;
