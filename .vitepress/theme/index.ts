dfbce98503b7278ae4a4d5d07288bf9511a60ce4
TossSky <vyacheslav@totsky.ee>
2025-08-18 16:54:06 +0300

    Updated daemon version in docs to 6.1.0

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -11,7 +11,7 @@ import type { Theme } from "vitepress";
 import { h } from "vue";
 import "./style.css";
 
-const DAEMON_VERSION = 'v6.0.0';
+const DAEMON_VERSION = 'v6.1.0';
 
 export default {
     extends: DefaultTheme,
